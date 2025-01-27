a5e4102d27a1ddf5091a748536f4d8b451028d01
mohamed2233996 <medomano771@gmail.com>
2025-01-27 16:46:58 +0200

    🐞 fix: useEffect in main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,26 @@
 'use client'
 import { useRouter } from "next/navigation";
 import Landing from "./_componant/Landing";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const router = useRouter();
-  useEffect(() => {
-    const isLoggedIn = localStorage.getItem('isLoggedIn');
+  const [isClient, setIsClient] = useState(false);
 
+  useEffect(() => {
+    // تأكد من أن الكود يعمل في المتصفح فقط
+    setIsClient(true);
+
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
     if (isLoggedIn) {
       router.push('/dashboard');
-      return null;
     }
-  }, []);
+  }, [router]);
 
+  // قم بالعرض فقط عندما نكون في بيئة المتصفح
+  if (!isClient) {
+    return null; // تجنب عرض المحتوى على الخادم أثناء مرحلة SSR
+  }
 
   return (
     <div>
