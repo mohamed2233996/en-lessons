b1c789d436bf932f9033d2d42040ed0f11cc2655
mohamed2233996 <medomano771@gmail.com>
2025-05-22 20:41:08 +0300

    🐞 fix: lang Referesh

--- src/app/_componant/LanguageSwitcher.jsx
+++ src/app/_componant/LanguageSwitcher.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const LanguageSwitcher = () => {
-
     const { i18n } = useTranslation();
-    
+
+    useEffect(() => {
+        const savedLang = localStorage.getItem('appLanguage');
+        if (savedLang && savedLang !== i18n.language) {
+            i18n.changeLanguage(savedLang);
+            document.documentElement.dir = savedLang === 'ar' ? 'rtl' : 'ltr';
+        } else {
+            // ضبط الاتجاه حسب اللغة الافتراضية أو الحالية
+            document.documentElement.dir = i18n.language === 'ar' ? 'rtl' : 'ltr';
+        }
+    }, [i18n]);
+
     const toggleLanguage = () => {
         const newLang = i18n.language === "en" ? "ar" : "en";
         i18n.changeLanguage(newLang);
         document.documentElement.dir = newLang === "ar" ? "rtl" : "ltr";
+        localStorage.setItem('appLanguage', newLang);  // حفظ اللغة المختارة
     };
 
     return (
