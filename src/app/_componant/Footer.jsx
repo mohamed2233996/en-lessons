997710861ab2b8a6d599fd75286df26e4828d8f9
mohamed2233996 <medomano771@gmail.com>
2025-06-28 14:29:36 +0300

    🐞 fix: add height attribute to Image component in Footer

--- src/app/_componant/Footer.jsx
+++ src/app/_componant/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <Link href="/" className="flex items-center">
-                        <Image src={logo} alt="logo" width={80} />
+                        <Image src={logo} alt="logo" width={80} height={80} />
                     </Link>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-dark sm:mb-0 dark:text-white">
                         <li>
